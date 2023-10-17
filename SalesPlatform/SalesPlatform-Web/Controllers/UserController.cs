ba5928070a323e380f24a94ae1e7d1706dd699c6
Yaroslav <vizichkanich0709@gmail.com>
2023-10-17 15:41:33 +0300

    new user controllers

---
+++ SalesPlatform/SalesPlatform-Web/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SalesPlatform_Application.IServices;
+
+namespace SalesPlatform_Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService; 
+        }
+
+        [HttpGet, Authorize]
+        public async Task<ActionResult> GetUserById(string userId)
+        {
+            var user = await _userService.GetUser(userId);
+            
+            return Ok(user);
+        }
+
+    }
+}
