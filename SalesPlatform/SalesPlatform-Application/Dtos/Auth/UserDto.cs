b4c6aa0ca37999d9f19cf2b4aefd7d0d03ee1a26
Yaroslav <vizichkanich0709@gmail.com>
2023-10-17 15:35:44 +0300

    new user dto

---
+++ SalesPlatform/SalesPlatform-Application/Dtos/Auth/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SalesPlatform_Application.Dtos.Auth
+{
+    public class UserDto
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
