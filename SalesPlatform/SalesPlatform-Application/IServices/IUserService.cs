a69302bcabddad6077ec85408ba5901555365699
Yaroslav <vizichkanich0709@gmail.com>
2023-10-17 15:38:54 +0300

    new service

---
+++ SalesPlatform/SalesPlatform-Application/IServices/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using SalesPlatform_Application.Dtos.Auth;
+
+namespace SalesPlatform_Application.IServices
+{
+    public interface IUserService
+    {
+        Task<UserDto> GetUser(string userId);
+    }
+}
---
+++ SalesPlatform/SalesPlatform-Application/Services/UserService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using SalesPlatform_Application.Dtos.Auth;
+using SalesPlatform_Application.IServices;
+using SalesPlatform_Domain.Entities.Identity;
+using SalesPlatform_Infrastructure.Repositories;
+
+namespace SalesPlatform_Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository<ApplicationUser> _userRepository;
+        private readonly IMapper _mapper;
+
+        public UserService(IRepository<ApplicationUser> userRepository,
+                           IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+
+        public async Task<UserDto> GetUser(string userId)
+        {
+            var user = await _userRepository.GetByIdAsync(userId);
+
+            return _mapper.Map<UserDto>(user);
+        }
+    }
+}
