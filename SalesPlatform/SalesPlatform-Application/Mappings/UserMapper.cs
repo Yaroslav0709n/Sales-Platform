efb290c7b6674eaa3003ee4df3b21257faa37742
Yaroslav <vizichkanich0709@gmail.com>
2023-10-17 15:39:29 +0300

    new user mapper

---
+++ SalesPlatform/SalesPlatform-Application/Mappings/UserMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SalesPlatform_Application.Dtos.Auth;
+using SalesPlatform_Domain.Entities.Identity;
+
+namespace SalesPlatform_Application.Mappings
+{
+    public class UserMapper:Profile
+    {
+        public UserMapper()
+        {
+            CreateMap<UserDto, ApplicationUser>();
+            CreateMap<ApplicationUser, UserDto>();
+
+        }
+    }
+}
