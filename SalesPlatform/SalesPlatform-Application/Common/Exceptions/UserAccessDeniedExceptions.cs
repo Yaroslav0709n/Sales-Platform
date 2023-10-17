989c91b1bbe23cd04251a0f323914e9e438370c6
Yaroslav <vizichkanich0709@gmail.com>
2023-10-17 15:34:52 +0300

    add user exception

---
+++ SalesPlatform/SalesPlatform-Application/Common/Exceptions/UserAccessDeniedExceptions.cs
@@ -0,0 +1,7 @@
+﻿namespace SalesPlatform_Application.Common.Exceptions
+{
+    public class UserAccessDeniedExceptions:Exception
+    {
+        public UserAccessDeniedExceptions(string name) : base($"User: {name} access denied!") { }
+    }
+}
