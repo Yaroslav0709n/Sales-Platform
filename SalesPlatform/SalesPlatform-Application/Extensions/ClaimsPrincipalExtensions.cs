ff2aa874ec34147506e7f9b933614d510911ee2f
Yaroslav <vizichkanich0709@gmail.com>
2023-10-17 15:37:12 +0300

    extension for current userid

---
+++ SalesPlatform/SalesPlatform-Application/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,25 @@
+﻿using SalesPlatform_Application.Common.Exceptions;
+using System.Security.Claims;
+
+namespace SalesPlatform_Application.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static Guid GetCurrentUserId(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+            {
+                throw new ArgumentNullException(nameof(principal));
+            }
+            var result = Guid.TryParse(
+                principal.Claims
+                    .FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier
+                )?.Value, out var userId);
+            if (!result)
+            {
+                throw new UserAccessDeniedExceptions(principal.ToString());
+            }
+            return userId;
+        }
+    }
+}
