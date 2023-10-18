d472fda61cf9ea020b4b48afccf59d32db37e13e
Yaroslav <vizichkanich0709@gmail.com>
2023-10-18 23:54:21 +0300

    rename file

---
+++ SalesPlatform/SalesPlatform-Application/Dtos/User/RegisterDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesPlatform_Application.Dtos.Auth
+{
+    public class RegisterDto
+    {
+        [Required(ErrorMessage = "First name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        public string PhoneNumber { get; set; }
+
+        public string? City { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
