ac79036b8994da71e1807626dfa3b3de9a924887
Yaroslav <vizichkanich0709@gmail.com>
2023-10-18 23:52:27 +0300

    new models

--- SalesPlatform/SalesPlatform-Infrastructure/Context/ApplicationDbContext.cs
+++ SalesPlatform/SalesPlatform-Infrastructure/Context/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SalesPlatform_Domain.Entities;
 using SalesPlatform_Domain.Entities.Identity;
 
 namespace SalesPlatform_Infrastructure.Context
@@ -10,6 +11,8 @@ namespace SalesPlatform_Infrastructure.Context
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options){}
 
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
-
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<ItemCategory> ItemCategories { get; set; }
     }
 }
