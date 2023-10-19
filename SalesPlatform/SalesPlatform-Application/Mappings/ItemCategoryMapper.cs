e8c195506888360d53a81e8357e4a412f276e49d
Yaroslav <vizichkanich0709@gmail.com>
2023-10-19 23:51:06 +0300

    init commit

---
+++ SalesPlatform/SalesPlatform-Application/Mappings/ItemCategoryMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SalesPlatform_Application.Dtos.Item;
+using SalesPlatform_Application.Dtos.ItemCategory;
+using SalesPlatform_Domain.Entities;
+
+namespace SalesPlatform_Application.Mappings
+{
+    public class ItemCategoryMapper:Profile
+    {
+        public ItemCategoryMapper()
+        {
+            CreateMap<ItemCategoryDto, ItemCategory>();
+            CreateMap<ItemCategory, ItemCategoryDto>();
+        }
+    }
+}
---
+++ SalesPlatform/SalesPlatform-Application/Mappings/ItemMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SalesPlatform_Application.Dtos.Item;
+using SalesPlatform_Domain.Entities;
+
+namespace SalesPlatform_Application.Mappings
+{
+    public class ItemMapper:Profile
+    {
+        public ItemMapper()
+        {
+            CreateMap<ItemDto, Item>();
+            CreateMap<Item, ItemDto>();
+
+            CreateMap<UpdateItemDto, Item>();
+            CreateMap<Item, UpdateItemDto>();
+        }
+    }
+}
