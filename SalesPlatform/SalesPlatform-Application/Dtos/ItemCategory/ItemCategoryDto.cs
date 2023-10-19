17e9d713d16f3f622c63e3c412bc3379cb6a274f
Yaroslav <vizichkanich0709@gmail.com>
2023-10-19 23:48:26 +0300

    new category dto

---
+++ SalesPlatform/SalesPlatform-Application/Dtos/ItemCategory/ItemCategoryDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesPlatform_Application.Dtos.ItemCategory
+{
+    public class ItemCategoryDto
+    {
+        public string Name { get; set; }
+    }
+}
