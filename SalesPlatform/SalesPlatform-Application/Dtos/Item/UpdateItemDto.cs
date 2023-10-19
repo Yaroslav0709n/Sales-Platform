66e22c31c97ec730b1e3cd53d8179564e319aa3e
Yaroslav <vizichkanich0709@gmail.com>
2023-10-19 23:47:12 +0300

    new update item dto

---
+++ SalesPlatform/SalesPlatform-Application/Dtos/Item/UpdateItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SalesPlatform_Application.Dtos.Item
+{
+    public class UpdateItemDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
