ca41d146fdf1f4d093ef033264cbec9ada64e22b
Yaroslav <vizichkanich0709@gmail.com>
2023-10-20 21:34:03 +0300

    init dtos validators

---
+++ SalesPlatform/SalesPlatform-Web/Extensions/ValidationServicesExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using SalesPlatform_Application.Dtos.Auth;
+using SalesPlatform_Application.Dtos.Item;
+using SalesPlatform_Application.Validation;
+
+namespace SalesPlatform_Web.Extensions
+{
+    public static class ValidationServicesExtensions
+    {
+        public static IServiceCollection AddValidationService(this IServiceCollection services)
+        {
+            services.AddTransient<IValidator<RegisterDto>, RegisterUserValidator>();
+            services.AddTransient<IValidator<ItemDto>, ItemValidator>();
+            services.AddTransient<IValidator<UpdateItemDto>, UpdateItemValidator>();
+
+            return services;
+        }
+    }
+}
