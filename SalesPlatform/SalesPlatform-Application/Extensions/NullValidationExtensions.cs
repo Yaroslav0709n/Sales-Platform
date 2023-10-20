414b4d40884de2695ff4aea41c0401b6aecada7d
Yaroslav <vizichkanich0709@gmail.com>
2023-10-20 21:31:03 +0300

    null validation

---
+++ SalesPlatform/SalesPlatform-Application/Extensions/NullValidationExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace SalesPlatform_Application.Extensions
+{
+    public static class NullValidationExtensions
+    {
+        public static T ThrowIfNull<T>(this T argument, string argumentName)
+        {
+            if (argument == null)
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+
+            return argument;
+        }
+    }
+}
