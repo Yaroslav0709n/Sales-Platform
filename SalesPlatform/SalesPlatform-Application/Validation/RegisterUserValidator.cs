acd357ef9c9dc6d0369d5910715b2b97b83bd5a2
Yaroslav <vizichkanich0709@gmail.com>
2023-10-20 21:31:56 +0300

    validators for dto

---
+++ SalesPlatform/SalesPlatform-Application/Validation/RegisterUserValidator.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+using SalesPlatform_Application.Dtos.Auth;
+
+namespace SalesPlatform_Application.Validation
+{
+    public class RegisterUserValidator : AbstractValidator<RegisterDto>
+    {
+        public RegisterUserValidator()
+        {
+            RuleFor(register => register.Email)
+                .EmailAddress()
+                .WithMessage("'Email' field is not a valid e-mail address");
+
+            RuleFor(register => register.FirstName)
+                .NotEmpty()
+                .WithMessage("'First name' must be filled")
+                .MaximumLength(250)
+                .WithMessage("'First name' must be no longer than 250 characters");
+
+            RuleFor(register => register.LastName)
+                .NotEmpty()
+                .WithMessage("'Last name' must be filled")
+                .MaximumLength(250)
+                .WithMessage("'Last name' must be no longer than 250 characters");
+
+            RuleFor(register => register.City)
+                .NotEmpty()
+                .WithMessage("'City' must be filled")
+                .MinimumLength(50)
+                .WithMessage("'City' must be at least 50 characters long");
+
+            RuleFor(register => register.PhoneNumber)
+                .NotEmpty()
+                .WithMessage("'Phone number' must be filled")
+                .MinimumLength(20)
+                .WithMessage("'City' must be at least 20 characters long");
+
+            RuleFor(register => register.Password)
+                .NotEmpty()
+                .WithMessage("'Password' must be filled")
+                .MinimumLength(6)
+                .WithMessage("'Password' must be at least 6 characters long");
+        }
+    }
+}
---
+++ SalesPlatform/SalesPlatform-Application/Validation/UpdateItemValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using SalesPlatform_Application.Dtos.Item;
+
+namespace SalesPlatform_Application.Validation
+{
+    public class UpdateItemValidator : AbstractValidator<UpdateItemDto>
+    {
+        public UpdateItemValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("'Name' must be filled")
+                .MaximumLength(50)
+                .WithMessage("'Name' must be no longer than 40 characters");
+
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .WithMessage("'Description' must be filled")
+                .MaximumLength(1000)
+                .WithMessage("'Description' must be no longer than 1000 characters");
+
+            RuleFor(x => x.City)
+                .NotEmpty()
+                .WithMessage("'City' must be filled")
+                .MaximumLength(50)
+                .WithMessage("'City' must be no longer than 50 characters");
+        }
+    }
+}
