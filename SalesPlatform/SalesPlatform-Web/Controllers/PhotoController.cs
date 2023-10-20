f4a4638b6cafd34773bb1060c8ac30a25453e3a2
Yaroslav <vizichkanich0709@gmail.com>
2023-10-20 21:32:38 +0300

    new photo controller

---
+++ SalesPlatform/SalesPlatform-Web/Controllers/PhotoController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SalesPlatform_Application.IServices;
+
+namespace SalesPlatform_Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : ControllerBase
+    {
+        private readonly IPhotoService _photoService;
+
+        public PhotoController(IPhotoService photoService)
+        {
+            _photoService = photoService;
+        }
+
+        [HttpGet, Authorize]
+        public async Task<ActionResult> GetPhotosByItemId(int itemId)
+        {
+            var photos = await _photoService.GetPhotosByItemIdAsync(itemId);
+
+            return Ok(photos);
+        }
+
+        [HttpPost, Authorize]
+        public async Task<ActionResult> UploadPhotos(int itemId, List<IFormFile> files)
+        {
+            var photos = await _photoService.UploadPhotosAsync(itemId, files);
+
+            return Ok(true);
+        }
+
+        [HttpPut, Authorize]
+        public async Task<ActionResult> UpdatePhoto(int photoId, IFormFile file)
+        {
+            var photo = await _photoService.UpdatePhotoAsync(photoId, file);
+
+            return Ok(true);
+        }
+
+        [HttpDelete, Authorize]
+        public async Task<ActionResult> DeletePhoto(int photoId)
+        {
+            var photo = await _photoService.DeletePhotoAsync(photoId);
+
+            return Ok(true);
+        }
+    }
+}
