82d57a90c223883ca44b28aedf8bf5c2d15188ef
Yaroslav <vizichkanich0709@gmail.com>
2023-10-25 20:15:06 +0300

    init chathub

---
+++ SalesPlatform/SalesPlatform-Application/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SalesPlatform_Application.Hubs
+{
+    public class ChatHub:Hub 
+    {
+        public async Task SendMessage(int chatId, string userId, string messageText)
+        {
+            await Clients.Group(chatId.ToString()).SendAsync("ReceiveMessage", userId, messageText);
+        }
+    }
+}
