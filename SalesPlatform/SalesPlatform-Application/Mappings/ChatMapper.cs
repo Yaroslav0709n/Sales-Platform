56ac277faa4dfce39b7d205425195a7808b0e8bb
Yaroslav <vizichkanich0709@gmail.com>
2023-10-25 20:16:47 +0300

    new chat mapper

---
+++ SalesPlatform/SalesPlatform-Application/Mappings/ChatMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SalesPlatform_Application.Dtos.Chat;
+using SalesPlatform_Domain.Entities.Chats;
+
+namespace SalesPlatform_Application.Mappings
+{
+    public class ChatMapper:Profile
+    {
+        public ChatMapper()
+        {
+            CreateMap<ChatDto, Chat>();
+            CreateMap<Chat, ChatDto>();
+
+            CreateMap<MessageDto, Message>();
+            CreateMap<Message, MessageDto>();
+        }
+    }
+}
