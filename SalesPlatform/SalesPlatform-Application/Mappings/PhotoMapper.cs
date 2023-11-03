40f95d2f8b443e04d998158a0a2765ce01f640f3
Yaroslav <vizichkanich0709@gmail.com>
2023-11-04 00:28:45 +0200

    new photo mapper

---
+++ SalesPlatform/SalesPlatform-Application/Mappings/PhotoMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using SalesPlatform_Application.Dtos;
+using SalesPlatform_Domain.Entities;
+
+namespace SalesPlatform_Application.Mappings
+{
+    public class PhotoMapper:Profile
+    {
+        public PhotoMapper()
+        {
+            CreateMap<PhotoDto, Photo>();
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
