4a3c3018e8747c0e844f2bdb13c8ec91f724ee5e
Yaroslav <vizichkanich0709@gmail.com>
2023-11-04 00:32:15 +0200

    create interfaces

---
+++ SalesPlatform/SalesPlatform-Frontend/app/src/interfaces/Item/Item.ts
@@ -0,0 +1,10 @@
+export interface Item {
+    $id: string,
+    id: string,
+    name: string;
+    description: string;
+    price: number;
+    time: string;
+    city: string,
+    state: string
+}
