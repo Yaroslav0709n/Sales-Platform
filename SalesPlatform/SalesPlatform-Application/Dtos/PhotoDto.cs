0fc37b50eb57741704facb2053dd26c9b821a4f6
Yaroslav <vizichkanich0709@gmail.com>
2023-11-04 00:28:11 +0200

    new photodto

---
+++ SalesPlatform/SalesPlatform-Application/Dtos/PhotoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesPlatform_Application.Dtos
+{
+    public class PhotoDto
+    {
+        public int Id { get; set; }
+        public byte[] ImageData { get; set; }
+        public string Caption { get; set; }
+    }
+}
